1c6cd4c1b4cdf2ef52f8504b94afa810c081117b
Binay Timilsina <binayprogramming@gmail.com>
2022-07-03 18:26:20 +0200

    Made dummy function to handle category filters

--- src/components/FilterForm/FilterForm.js
+++ src/components/FilterForm/FilterForm.js
@@ -5,6 +5,17 @@ import './styles.css'
 const categories = ['people', 'premium', 'pets', 'food', 'landmarks', 'cities', 'nature'];
 const priceRange = ['Lower than $20', '$20 - $100', '$100 - $200', 'More than $200'];
 
+const categoriesSelected = []
+
+const categoriesSelectionHandler = (e) => {
+    if (categoriesSelected.includes(e)) {
+        categoriesSelected.splice(categoriesSelected.indexOf(e), 1);
+    } else {
+        categoriesSelected.push(e)
+    }
+    console.log(categoriesSelected)
+}
+
 
 export const CategoryForm = () => {
     return (
@@ -14,7 +25,7 @@ export const CategoryForm = () => {
                 {categories.map(el => {
                     return (
                         <div style={{ marginBottom: '15px' }}>
-                            <input type="checkbox" id={el} name={el} value={el} />
+                            <input type="checkbox" id={el} name={el} value={el} onClick={() => categoriesSelectionHandler(el)} />
                             <label for={el} className="label__css" >{el}</label><br />
                         </div>
                     )
