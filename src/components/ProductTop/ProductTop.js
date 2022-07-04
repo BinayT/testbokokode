f6c33cdc173de93c13c05cd0a605a74a072f772a
Binay Timilsina <binayprogramming@gmail.com>
2022-07-04 13:41:44 +0200

    Fixed CSS for better UI on product list

--- src/components/ProductTop/ProductTop.js
+++ src/components/ProductTop/ProductTop.js
@@ -5,7 +5,7 @@ import Sort from '../SortComponent/Sort'
 
 const ProductTop = () => {
     return (
-        <div className='product-top'>
+        <div className='product__top'>
             <p><span className='photography__text'>Photography / </span><span className='premium__photo__text'>Premium Photos</span></p>
             <Sort />
         </div>
